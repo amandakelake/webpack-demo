06b5f63d9d590e790fc22764accb3480139e5c4e
amandakelake <930007017@qq.com>
2020-01-07 09:18:18 +0800

    feat: makeDependenciesGraph

--- bundler.js
+++ bundler.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+// 把JS代码转为AST（抽象语法树，可以简单google一下概念，先不用太深入）
 const parser = require('@babel/parser');
+// 帮助我们解析AST的内容，最直接的就是通过 ImportDeclaration 点位找到文件的依赖入口
 const traverse = require('@babel/traverse').default;
+// babel.transformFromAst(AST, code, options) 可以帮助我们把AST转换成能ES5代码
 const babel = require('@babel/core');
 
 // 入口文件模块分析
@@ -16,9 +19,9 @@ const moduleAnalyse = filename => {
     traverse(ast, {
         ImportDeclaration({ node }) {
             const dirname = path.dirname(filename);
-            // node.source.value就是模块名
-            // 遍历获取模块的绝对路径（相对于打包目录的绝对路径）
-            dependencies[node.source.value] = path.join(dirname, node.source.value);
+            // node.source.value就是获取到的模块路径名，带有相对于当前文件的路径
+            // 比如import sleep from './utils/sleep.js'里面的'./utils/sleep.js'
+            dependencies[node.source.value] = `./${path.join(dirname, node.source.value)}`;
         },
     });
     // babel翻译AST为浏览器可以识别的代码
@@ -32,5 +35,33 @@ const moduleAnalyse = filename => {
     };
 };
 
-const moduleInfo = moduleAnalyse('./src/index.js');
-console.log('moduleInfo', moduleInfo);
+const makeDependenciesGraph = entry => {
+    // 先拿到入口文件的模块分析对象
+    const entryModule = moduleAnalyse(entry);
+    // 将通过递归遍历，把所有的模块依赖收集到这里
+    const graphArray = [entryModule];
+    for (let i = 0; i < graphArray.length; i++) {
+        const item = graphArray[i];
+        const { dependencies } = item;
+        // 判断dependencies对象是否为空，即item是否还有依赖
+        if (Object.keys(dependencies).length > 0) {
+            for (let j in dependencies) {
+                // 把得到的子依赖添加进graphArray，长度发生变化，for循环继续，形成了递归
+                graphArray.push(moduleAnalyse(dependencies[j]));
+            }
+        }
+    }
+    // 数组转换为对象  方便后续操作
+    const graph = {};
+    graphArray.forEach(item => {
+        graph[item.filename] = {
+            dependencies: item.dependencies,
+            code: item.code,
+        };
+    });
+
+    return graph;
+};
+
+const graphInfo = makeDependenciesGraph('./src/index.js');
+console.log('graphInfo', graphInfo);
---
+++ src/bundler-utils/name.js
@@ -0,0 +1,2 @@
+const name = 'LGC';
+export { name };
